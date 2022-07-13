fe564e682e593bf4de792288aa700287b58f3e2c
Caio Ribeiro <caioscarpim@hotmail.com>
2022-07-13 10:50:34 -0300

    segunda parte

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let tamanhoSenha = document.querySelector("#tamanhoSenha");
+let buttonSenha = document.querySelector("#gerar");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#senha");
+
+let containerPassword = documet.querySelector(".container-password");
+
+let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+let novaSenha = "";
